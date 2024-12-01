70b32c9f07751a8e4bcf34c128c55b75e7ecf45c
NSavenije <noud.savenije@hotmail.com>
2024-12-01 06:24:36 +0100

    initial 1b

--- src/Day1/Trebuchet.cs
+++ src/Day1/Trebuchet.cs
@@ -14,6 +14,7 @@ static class Day1 {
         int res = 0;
         List<int> list1 = [];
         List<int> list2 = [];
+        Dictionary<int, int> groups = [];
         while ((line = sr.ReadLine()) != null)
         {
             // Console.WriteLine(line);
@@ -24,11 +25,20 @@ static class Day1 {
         }
         list1.Sort();
         list2.Sort();
+        for(int i = 0; i < list2.Count; i++)
+        {
+            if (groups.TryGetValue(list2[i], out _))
+                groups[list2[i]]++;
+            else
+                groups[list2[i]] = 1;
+        }
         for(int i = 0; i < list1.Count; i++)
         {
             // Console.WriteLine(list1[i]);
             // Console.WriteLine(list2[i]);
-            res += Math.Abs(list1[i] - list2[i]);
+            // res += Math.Abs(list1[i] - list2[i]);
+            if (groups.TryGetValue(list1[i], out var val))
+                res += list1[i] * groups[list1[i]];
         }
         Console.WriteLine(res);
     }
