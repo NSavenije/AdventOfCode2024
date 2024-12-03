b88a2ba9da515775e9cd4ff39bd811cc5c8fe3ea
Noud Savenije <noud.savenije@hotmail.com>
2024-12-03 06:55:53 +0100

    cleanup

--- src/Day3/muls.cs
+++ src/Day3/muls.cs
@@ -4,28 +4,21 @@ using System;
 using System.Text.RegularExpressions;
 
 static class Day3 {
-    public static void Solve1() {
-        ParseInput();
-    }
+    public static void Solve1() =>
+        ParseInput(true);
         
-    public static void Solve2() 
-    {
-        
-    }
+    public static void Solve2() => 
+        ParseInput(false);
 
-    private static void ParseInput()
+    private static void ParseInput(bool part1)
     {
         string filePath = "src/Day3/3.in";
         using StreamReader sr = new(filePath);
         string line;
-        List<string> muls = [];
 
-        // var pattern = @"mul\(\d{1,3},\d{1,3}\)|do\(\)|don\'t\(\)";
         var pattern = @"(mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\))";
-
-        
         Regex regex = new(pattern); 
-        
+        bool enabled = true;
         int res = 0;
 
         while ((line = sr.ReadLine()) != null)
@@ -33,26 +26,17 @@ static class Day3 {
             MatchCollection matches = regex.Matches(line);
             foreach(Match match in matches)
             {
-                muls.Add(match.Value);
-            }
-            
-        }
-
-        bool enabled = true;
-        foreach(string mulExp in muls)
-        {
-            Console.WriteLine(mulExp);
-            
-            if (mulExp == "do()")
-                enabled = true;
-            else if (mulExp == "don't()")
-                enabled = false;
-            else if (enabled) {
-            string mul = mulExp[4..^1];
-            // Console.WriteLine(mul + " " + muls.Count);
-
-            int[] nums = mul.Split(',').Select(int.Parse).ToArray();
-            res += nums[0] * nums[1];
+                string mulExp = match.Value;
+                if (mulExp == "do()")
+                    enabled = true;
+                else if (mulExp == "don't()")
+                    enabled = false;
+                else if (enabled || part1) 
+                {
+                    string mul = mulExp[4..^1];
+                    int[] nums = mul.Split(',').Select(int.Parse).ToArray();
+                    res += nums[0] * nums[1];
+                }
             }
         }
         Console.WriteLine(res);
