c555903ce4c86109739b6e0340a112bc2c003846
Noud Savenije <noud.savenije@hotmail.com>
2024-12-07 06:54:19 +0100

    few improvements

--- src/Day7/BridgeRepair.cs
+++ src/Day7/BridgeRepair.cs
@@ -5,7 +5,11 @@ using System.Text;
 
 static class Day7 {
     
-    public static void Solve1() 
+    public static void Solve1() => ParseInput(false);
+
+    public static void Solve2() => ParseInput(true);
+
+    static void ParseInput(bool allowConcat)
     {
         string filePath = "src/Day7/7.in";
         using StreamReader sr = new(filePath);
@@ -17,43 +21,35 @@ static class Day7 {
             long target = long.Parse(split[0]);
             List<int> vars = split[1].Split(' ').Select(int.Parse).ToList();
 
-            bool valid = CanReachTarget(vars, target);
-            if (valid)
+            if (CanReachTarget(vars, target, allowConcat))
             {
                 res += target;
             }
-
         }
         Console.WriteLine(res);
     }
 
-    public static void Solve2()
-    {
-    
-    }
+    static bool CanReachTarget(List<int> vars, long target, bool allowConcat) =>
+        CheckCombinations(vars, 0, vars[0], target, allowConcat);
 
-    static bool CanReachTarget(List<int> vars, long target) 
-    {   
-        return CheckCombinations(vars, 0, vars[0], target);
-    } 
-    static bool CheckCombinations(List<int> vars, int index, long current, long target) 
+    static bool CheckCombinations(List<int> vars, int index, long current, long target, bool allowConcat) 
     {   
         if (index == vars.Count - 1) 
         {   
             return current == target;
         } 
         // Trying addition 
-        if (CheckCombinations(vars, index + 1, current + vars[index + 1], target)) 
+        if (CheckCombinations(vars, index + 1, current + vars[index + 1], target, allowConcat)) 
         {   
             return true;
         } 
         // Trying multiplication 
-        if (CheckCombinations(vars, index + 1, current * vars[index + 1], target)) 
+        if (CheckCombinations(vars, index + 1, current * vars[index + 1], target, allowConcat)) 
         {   
             return true;
         } 
         // Trying concatenation
-        if (CheckCombinations(vars, index + 1, Concat(current, vars[index + 1]), target)) 
+        if (allowConcat && CheckCombinations(vars, index + 1, Concat(current, vars[index + 1]), target, allowConcat)) 
         {   
             return true;
         }
