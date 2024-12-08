6ad172951dc50d7a347246f28201276ebc4aec4e
Noud Savenije <noud.savenije@hotmail.com>
2024-12-08 07:24:07 +0100

    cleanup

--- src/Day8/Antenaa.cs
+++ src/Day8/Antenaa.cs
@@ -1,51 +1,42 @@
 #nullable disable
 
-using System.Data;
-using System.Text;
-
 static class Day8 {
     
-    public static void Solve1() => ParseInput(false);
+    public static void Solve1() => Solve(false);
 
-    public static void Solve2() => ParseInput(true);
+    public static void Solve2() => Solve(true);
 
-    static void ParseInput(bool harmonics)
+    static void Solve(bool harmonics)
     {
-        Dictionary<char, List<(int x,int y)>> map = [];
         string filePath = "src/Day8/8.in";
         List<string> input = File.ReadAllLines(filePath).ToList();  
+
+        Dictionary<char, List<(int x,int y)>> map = [];
         for(int x = 0; x < input.Count; x++)
         {
             for(int y = 0; y < input.Count; y++)
             {
                 char test = input[y][x];
-                if (test != '.')
-                {
-                    if (map.TryGetValue(test, out var values))
-                    {
-                        values.Add((x,y));
-                        map[test] = values;
-                    }
-                    else
-                    {
-                        map.Add(test, [(x,y)]);
-                    }
-                }
+                if (test == '.')
+                    continue;
+                
+                if (!map.TryGetValue(test, out var values))
+                    values = [];
+
+                values.Add((x,y));
+                map[test] = values;
             }
         }
 
         HashSet<(int x, int y)> antiNodes = [];
         foreach(var key in map.Keys)
         {
-            if (!harmonics)
-                antiNodes.UnionWith(GetAntiNodes(map[key], 0, input.Count));
-            else
-                antiNodes.UnionWith(GetHarmonicAntiNodes(map[key], 0, input.Count));
+            antiNodes.UnionWith(GetAntiNodes(map[key], 0, input.Count, harmonics));
         }
         Console.WriteLine(antiNodes.Count);
     }
 
-    static HashSet<(int,int)> GetAntiNodes(List<(int x, int y)> nodes, int min, int max)
+    static HashSet<(int,int)> GetAntiNodes(List<(int x, int y)> nodes, int min, int max, bool harmonics)
     {
         HashSet<(int, int)> antiNodes = [];
 
@@ -55,57 +46,33 @@ static class Day8 {
             {
                 var n1 = nodes[i];
                 var n2 = nodes[j];
-                int dx1 = n1.x - n2.x;
-                int dx2 = n2.x - n1.x;
-                int dy1 = n1.y - n2.y;
-                int dy2 = n2.y - n1.y;
-                int x1 = n1.x + dx1;
-                int y1 = n1.y + dy1;
-                int x2 = n2.x + dx2;
-                int y2 = n2.y + dy2;
-                if (x1 >= min && x1 < max && y1 >= min && y1 < max)
-                    antiNodes.Add((x1,y1));
-                if (x2 >= min && x2 < max && y2 >= min && y2 < max)
-                    antiNodes.Add((x2,y2));
-            }
-        }
-
-        return antiNodes;
-    }
-
-    static HashSet<(int,int)> GetHarmonicAntiNodes(List<(int x, int y)> nodes, int min, int max)
-    {
-        HashSet<(int, int)> antiNodes = [];
-
-        for(int i = 0; i < nodes.Count; i++)
-        {
-            for(int j = i + 1; j < nodes.Count; j++)
-            {
-                var n1 = nodes[i];
-                var n2 = nodes[j];
-                int dx1 = n1.x - n2.x;
-                int dx2 = n2.x - n1.x;
-                int dy1 = n1.y - n2.y;
-                int dy2 = n2.y - n1.y;
-                int x1 = n1.x;
-                int y1 = n1.y;
-                int x2 = n2.x;
-                int y2 = n2.y;
-                while (x1 >= min && x1 < max && y1 >= min && y1 < max)
-                {   
-                    antiNodes.Add((x1,y1));
-                    x1 += dx1;
-                    y1 += dy1;
-                }
-                while (x2 >= min && x2 < max && y2 >= min && y2 < max)
-                {
-                    antiNodes.Add((x2,y2));
-                    x2 += dx2;
-                    y2 += dy2;
+                int[] dx = [n1.x - n2.x, n2.x - n1.x];
+                int[] dy = [n1.y - n2.y, n2.y - n1.y];
+                int[] x = [n1.x, n2.x];
+                int[] y = [n1.y, n2.y];
+                for (int k = 0; k < 2; k++) 
+                { 
+                    if(!harmonics)
+                    {
+                        x[k] += dx[k]; 
+                        y[k] += dy[k];
+                        if (x[k] >= min && x[k] < max && y[k] >= min && y[k] < max) 
+                        { 
+                            antiNodes.Add((x[k], y[k]));
+                        }
+                    }
+                    else
+                    {
+                        while (x[k] >= min && x[k] < max && y[k] >= min && y[k] < max) 
+                        { 
+                            antiNodes.Add((x[k], y[k]));
+                            x[k] += dx[k];
+                            y[k] += dy[k];
+                        }
+                    }
                 }
             }
         }
-
         return antiNodes;
     }
 }
