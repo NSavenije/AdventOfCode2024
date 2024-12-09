0b96fdab1bec93671cb9ea898d30b11b919fab6c
Noud Savenije <noud.savenije@hotmail.com>
2024-12-09 10:28:38 +0100

    9b

--- src/Day9/Fragmenter.cs
+++ src/Day9/Fragmenter.cs
@@ -7,32 +7,29 @@ static class Day9 {
     
     public static void Solve1()
     {
-        string filePath = "src/Day9/9.in";
-        string disk = File.ReadAllLines(filePath)[0];
-        int[] blocks = ParseInput(disk);
-        Console.WriteLine(ToString(blocks));
-
-        int[] defragmented = Defragment(blocks);
-        Console.WriteLine(ToString(defragmented));
-
+        int[] blocks = ParseInput("src/Day9/9.in");
+        Defragment(blocks);
         long res = CalcChecksum(blocks);
         Console.WriteLine(res);
     }
 
     public static void Solve2()
     {
-
+        int[] blocks = ParseInput("src/Day9/9.in");
+        DefragmentBlocks(blocks);
+        long res = CalcChecksum(blocks, true);
+        Console.WriteLine(res);
     }
 
-    static int[] ParseInput(string disk)
+    static int[] ParseInput(string filePath)
     {
+        string disk = File.ReadAllLines(filePath)[0];
         List<int> blocks = [];
         int counter = 0;
         foreach(char c in disk)
         {
             int n = c - 48; //ASCII
-            // Generate a list 
-            int val = counter % 2 == 0 ? counter / 2 : -1;
+            int val = counter % 2 == 0 ? counter / 2 : -1 * counter;
             for(int i = 0; i < n; i++)
             {
                 blocks.Add(val);
@@ -61,14 +58,89 @@ static class Day9 {
         return blocks;
     }
 
-    static long CalcChecksum(int[] blocks)
+    static int[] DefragmentBlocks(int[] blocks)
+    {
+        Dictionary<int, (int index, int size)> blockDict = [];
+        Dictionary<int, (int index, int size)> spaceDict = [];
+        bool inBlock = true;
+        int si = blocks.Length - 1;
+        for(int i = blocks.Length - 2; i > 0; i--)
+        {
+            if (blocks[i] < blocks[i + 1] && inBlock)
+            {
+                blockDict.Add(blocks[i+1], ( si , si - i ));
+                inBlock = false;
+            }
+            if (!inBlock && blocks[i] >= 0)
+            {
+                inBlock = true;
+                si = i;
+            }
+        }
+
+        inBlock = true;
+        for(int i = 0; i < blocks.Length - 1; i++)
+        {
+            if (inBlock && blocks[i] < 0)
+            {
+                inBlock = false;
+                si = i;
+            }
+            if(!inBlock && blocks[i] >= 0)
+            {
+                inBlock = true;
+                spaceDict.Add(blocks[i - 1], ( si , i - si ));
+            }
+        }
+
+        foreach(var block in blockDict.Keys)
+        {
+            // Find first available space
+            var (index, size) = blockDict[block];
+            (int index, int size) sp = (0,0);
+            int spk = 0;
+            bool spaceFound = false;
+            foreach(var space in spaceDict.Keys)
+            {
+                sp = spaceDict[space];
+                if (sp.index >= index)
+                    break;
+                if (sp.size >= size)
+                {
+                    spaceFound = true;
+                    spk = space;
+                    break;
+                }
+            }
+            if(spaceFound)
+            {
+                // Move
+                for(int i = sp.index; i < sp.index + size; i++)
+                {
+                    blocks[i] = block;
+                }
+                for(int i = index; i > index - size; i--)
+                {
+                    blocks[i] = -1;
+                }
+
+                // Update SpaceDict
+                spaceDict[spk] = ( sp.index + size , sp.size - size);
+            }
+        } 
+        return blocks;
+    }
+
+    static long CalcChecksum(int[] blocks, bool gaps = false)
     {
         long res = 0;
-        List<int> ints = blocks.Where(x => x >= 0).ToList();
+        List<int> ints = [];
+        ints = gaps ? blocks.ToList() : blocks.Where(x => x >= 0).ToList();
+
         for(int i = 0; i < ints.Count; i++)
-        {
-            res += ints[i] * i;
-        }
+            if (blocks[i] >= 0)
+                res += ints[i] * i;
+
         return res;
     }
 
@@ -77,14 +149,8 @@ static class Day9 {
         StringBuilder result = new(); 
         foreach (int value in blocks) 
         { 
-            if (value >= 0) 
-            { 
-                result.Append(value); 
-            } 
-            else
-            {
-                result.Append('.'); 
-            } 
+            if (value >= 0) result.Append(value); 
+            else result.Append('.'); 
         } 
         return result.ToString();
     }
