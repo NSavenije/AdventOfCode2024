f72e24c1697bcdf3393a5aa074b6c7567340c20c
Noud Savenije <noud.savenije@hotmail.com>
2024-12-14 21:06:55 +0100

    terrible 14 2

--- src/Day14/RestroomRedout.cs
+++ src/Day14/RestroomRedout.cs
@@ -10,37 +10,159 @@ static class Day14
 
     static void Solve(bool part1)
     {
-        var robots = ParseInput("C:\\Users\\nouds\\Repos\\AdventOfCode2024\\src\\Day14\\14.in", part1);
-        int tl = 0;
-        int tr = 0;
-        int bl = 0;
-        int br = 0;
-        for(int r = 0; r < robots.Count; r++)
+        var robots = ParseInput("src/Day14/14.in", part1);
+        
+        for(int i = 0; true; i++)
         {
-            (int x,int y, int dx, int dy) = robots[r];
-            x = Mod(x + 100 * dx, 101);
-            y = Mod(y + 100 * dy, 103);
-            if (x < 50)
+            int tl = 0;
+            int tr = 0;
+            int bl = 0;
+            int br = 0;
+
+            int[] xCounts = new int[101];
+
+            for(int r = 0; r < robots.Count; r++)
             {
-                if (y < 51)
-                    tl++;
-                else if(y > 51)
-                    tr++;
+                (int x,int y, int dx, int dy) = robots[r];
+                x = Mod(x + dx, 101);
+                y = Mod(y + dy, 103);
+                robots[r] = (x,y,dx,dy);
+                xCounts[x]++;
+                if (x < 50)
+                {
+                    if (y < 51)
+                        tl++;
+                    else if(y > 51)
+                        tr++;
+                }
+                else if (x > 50)
+                {
+                    if (y < 51)
+                        bl++;
+                    else if(y > 51)
+                        br++;
+                }
             }
-            else if (x > 50)
+            
+            int score = tl * tr * bl * br;
+            if (part1 && i == 99)
             {
-                if (y < 51)
-                    bl++;
-                else if(y > 51)
-                    br++;
+                Console.WriteLine(score);
+                break;
+            }
+            int x1 = 0;
+            for(int r = 0; r < 101; r++)
+            {
+                if(xCounts[r] >= 30)
+                {
+                    int[] ys = new int[103];
+
+                    foreach(var rob in robots)
+                    {
+                        if(rob.x == r)
+                            ys[rob.y]++;
+                    }
+                    int streak = 0;
+                    int longestStreak = 0;
+                    for(int yy = 0; yy < 103; yy++)
+                    {
+                        if (ys[yy] > 0)
+                        {
+                            streak++;
+                            if (streak > longestStreak)
+                                longestStreak = streak;
+                        }
+                        else
+                        {
+                            streak = 0;
+                        }
+                    }
+                    if (longestStreak >= 30)
+                    {
+                        x1 = r;
+                        break;
+                    }
+                }
+            }
+            if (x1 == 0)
+                continue;
+            for(int r = x1; r < 103; r++)
+            {
+                if(xCounts[r] >= 30)
+                {
+                    int[] ys = new int[103];
+
+                    foreach(var rob in robots)
+                    {
+                        if(rob.x == r)
+                            ys[rob.y]++;
+                    }
+                    int streak = 0;
+                    int longestStreak = 0;
+                    for(int yy = 0; yy < 103; yy++)
+                    {
+                        if (ys[yy] > 0)
+                        {
+                            streak++;
+                            if (streak > longestStreak)
+                                longestStreak = streak;
+                        }
+                        else
+                        {
+                            streak = 0;
+                        }
+                    }
+                    if (longestStreak >= 30)
+                    {
+                        Console.WriteLine(i + 1);
+                        // ToString(robots);
+                        return;
+                    }
+                }
             }
         }
-        
-        int score = tl * tr * bl * br;
-        Console.WriteLine(score);
     }
 
     static int Mod(int x, int m) => (x % m + m) % m;
+
+    static void ToString(List<(int x, int y, int, int)> rs)
+    {
+        int rows = 101;
+        int columns = 103;
+        char[,] grid = new char[rows, columns];
+        // Initialize the grid with '.' 
+        for (int i = 0; i < rows; i++) 
+        { 
+            for (int j = 0; j < columns; j++) 
+            { 
+                grid[i, j] = '.';
+            } 
+        } 
+        // List of coordinates (x, y) to replace '.' with '1' 
+        // Replace specified coordinates with '1' 
+        foreach (var (x, y, _, _) in rs) 
+        { 
+            if (grid[x,y] == '.') 
+            { 
+                grid[x, y] = '1';
+            }
+            else
+            {
+                int c = (int)grid[x,y] - 48;
+                c++;
+                grid[x,y] = char.Parse(c.ToString());
+            } 
+        } 
+        // Print the grid 
+        for (int i = 0; i < rows; i++) 
+        { 
+            for (int j = 0; j < columns; j++) 
+            { 
+                Console.Write(grid[i, j]); 
+            } 
+            Console.WriteLine(); 
+        }
+    }
 
     static List<(int x,int y, int dx, int dy)> ParseInput(string filePath, bool part1) 
     { 
@@ -58,4 +180,5 @@ static class Day14
         }
         return robots;
     }
+
 }
