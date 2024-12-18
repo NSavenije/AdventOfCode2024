d47b66e2fd2f5fa5247dae0e670896e1177bbd1c
Noud Savenije <noud.savenije@hotmail.com>
2024-12-18 07:13:15 +0100

    ints needed to be longs.....

--- src/Day17/Chronospatial.cs
+++ src/Day17/Chronospatial.cs
@@ -2,38 +2,67 @@
 
 static class Day17
 {
-    // Register A: 729
-    // Register B: 0
-    // Register C: 0
-
-    // Program: 0,1,5,4,3,0
-
     public static void Solve1()
     {
         string filePath = "src/Day17/17.in";
         string[] lines = File.ReadAllLines(filePath);
-        int A = int.Parse(lines[0].Split(": ")[1]);
-        int B = int.Parse(lines[1].Split(": ")[1]);
-        int C = int.Parse(lines[2].Split(": ")[1]);
-        int[] operands = [0,1,2,3,A,B,C];
-        int[] program = lines[4].Split(": ")[1].Split(',').Select(int.Parse).ToArray();
-        // Console.WriteLine("A: " + A + " B: " +  B + " C: " + C + " P: " + string.Join(',', program));
-        List<int> output = [];
-        for(int i = 0; i < program.Length - 1; i += 2)
+        long A = int.Parse(lines[0].Split(": ")[1]);
+        long B = int.Parse(lines[1].Split(": ")[1]);
+        long C = int.Parse(lines[2].Split(": ")[1]);
+        long[] operands = [0,1,2,3,A,B,C];
+        long[] program = lines[4].Split(": ")[1].Split(',').Select(long.Parse).ToArray();
+        List<long> output = RunProgram(program, operands);
+        Console.WriteLine(string.Join(',', output));
+    }
+
+    public static void Solve2()
+    {
+        string filePath = "C:/Users/nouds/Repos/AdventOfCode2024/src/Day17/17.in";
+        string[] lines = File.ReadAllLines(filePath);
+        long[] program = lines[4].Split(": ")[1].Split(',').Select(long.Parse).ToArray();
+        int pointer = program.Length - 1;
+        long result = DFS(program, pointer, 0);
+        Console.WriteLine(result);
+        
+    }
+
+    static long DFS(long[] program, int cursor, long current)
+    {
+        for (int candidate = 0; candidate < 8; candidate++)
         {
-            int opcode = program[i];
-            int literal = program[i + 1];
-            int combo = operands[literal];
+            List<long> p1 = RunProgram(program, [0, 1, 2, 3, current * 8 + candidate, 0, 0]);
+            List<long> p2 = program.Skip(cursor).ToList();
+            if (p1.SequenceEqual(p2))
+            {
+                if (cursor == 0)
+                    return current * 8 + candidate;
+
+                var result = DFS(program, cursor - 1, current * 8 + candidate);
+                if (result > -1)
+                    return result;
+            }
+        }
+        return -1;
+    }
+
+    static List<long> RunProgram(long[] program, long[] operands)
+    {
+        List<long> output = [];
+        for(long i = 0; i < program.Length - 1; i += 2)
+        {
+            long opcode = program[i];
+            long literal = program[i + 1];
+            long combo = operands[literal];
             
             switch(opcode)
             {
                 case 0: // ADV / Division
-                    int num = operands[4];
+                    long num = operands[4];
                     double denom = Math.Pow(2, combo);
-                    operands[4] = (int)(num / denom);
+                    operands[4] = (long)(num / denom);
                     break;
                 case 1: // BXL bitwise XOR
-                    int b = operands[5];
+                    long b = operands[5];
                     operands[5] = b ^ literal;
                     break;  
                 case 2: // bst modulo
@@ -50,19 +79,14 @@ static class Day17
                     output.Add(combo % 8);
                     break;
                 case 6: // bdv division
-                    operands[5] = (int)(operands[4] / Math.Pow(2, combo));
+                    operands[5] = (long)(operands[4] / Math.Pow(2, combo));
                     break;
                 case 7: // cdv
-                    operands[6] = (int)(operands[4] / Math.Pow(2, combo));
+                    operands[6] = (long)(operands[4] / Math.Pow(2, combo));
                     break;
             }
         }
-        Console.WriteLine(string.Join(',', output));
-    }
-
-    public static void Solve2()
-    {
-        
+        return output;   
     }
 }
 
