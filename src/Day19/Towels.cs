d969796a8a0f4e411aa019df1b4cdbcdb80d7ff0
Noud Savenije <noud.savenije@hotmail.com>
2024-12-19 08:02:24 +0100

    19 2 with memo

--- src/Day19/Towels.cs
+++ src/Day19/Towels.cs
@@ -3,6 +3,7 @@
 static class Day19
 {
     static HashSet<string> Towels;
+    static Dictionary<string, long> memo = [];
 
     public static void Solve1()
     {
@@ -19,6 +20,20 @@ static class Day19
         Console.WriteLine(reachable.Count);
     }
 
+    public static void Solve2()
+    {
+        string filePath = "C:/Users/nouds/Repos/AdventOfCode2024/src/Day19/19.in";
+        string[] lines = File.ReadAllLines(filePath);
+
+        Towels = lines[0].Split(", ").ToHashSet();
+        long total = 0;
+        for(int i = 2; i < lines.Length; i++)
+        {
+            total += CountAllPatterns(lines[i], 0);
+        }
+        Console.WriteLine(total);
+    }
+
     static bool IsPatternPossible(string pattern, int cursor)
     {
         string remainder = pattern[cursor ..];
@@ -28,7 +43,16 @@ static class Day19
         return Towels.Any(towel => remainder.StartsWith(towel) && IsPatternPossible(pattern, cursor + towel.Length));
     }
 
-    public static void Solve2()
+    static long CountAllPatterns(string pattern, int cursor)
     {
+        string remainder = pattern[cursor ..];
+        if (memo.TryGetValue(remainder, out long rem))
+            return rem;
+        
+        long count = Towels.Contains(remainder) ? 1 : 0;
+        count += Towels.Where(remainder.StartsWith).Sum(towel => CountAllPatterns(pattern, cursor + towel.Length));
+
+        memo[remainder] = count;
+        return count;
     }
 }
