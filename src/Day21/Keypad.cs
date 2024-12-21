ff57d0ad42ffd7c7915c1800f749f42b0d1d08db
Noud Savenije <noud.savenije@hotmail.com>
2024-12-21 13:17:24 +0100

    day21 really clean!

--- src/Day21/Keypad.cs
+++ src/Day21/Keypad.cs
@@ -2,142 +2,100 @@
 
 static class Day21
 {
-    public static void Solve1()
+    public static void Solve1() => Solve(2);     
+
+    public static void Solve2() => Solve(25);
+
+    private static void Solve(int robots)
     {
-        string filePath = "C:/Users/nouds/Repos/AdventOfCode2024/src/Day21/21b.in";
+        string filePath = "src/Day21/21.in";
         string[] codes = File.ReadAllLines(filePath);
-        // Console.WriteLine(codes[0] + ": " + TranslateNumpadCode(codes[0]));
         long total = 0;
         foreach (string code in codes)
         {
-            string res = TranslateNumpadCode(code);
-            // Console.WriteLine(code + ": " + res);
-            res = TranslateKeypadCode(res);
-            // Console.WriteLine(code + ": " + res);
-            res = TranslateKeypadCode(res);
+            long keyPresses = GetKeyPresses(code, robots, numpad);
             int num = int.Parse(new string(code.Where(char.IsDigit).ToArray()));
-            Console.WriteLine(code + ": " + res);
-            total += res.Length * num;
+            total += keyPresses * num;
         }
         Console.WriteLine(total);
     }
 
-    public static void Solve2()
-    {
-        string filePath = "src/Day21/21.in";
-        string[] lines = File.ReadAllLines(filePath);
-    }
-
-    private static string TranslateNumpadCode(string code)
-    {
-        string res = "";
-        code = "A" + code;
-        for(int i = 0; i < code.Length - 1; i++)
-            res += TranslateNumpad(code[i], code[i + 1]);
-        return res;
-    }
-
-    private static string TranslateNumpad(char init, char goal)
-    {
-        // +---+---+---+
-        // | 7 | 8 | 9 |
-        // +---+---+---+
-        // | 4 | 5 | 6 |
-        // +---+---+---+
-        // | 1 | 2 | 3 |
-        // +---+---+---+
-        //     | 0 | A |
-        //     +---+---+
+    private static long GetKeyPresses(string code, long robots, Dictionary<char, (int r, int c)> input) 
+    { 
+        if (memo.TryGetValue((code,robots), out long val)) 
+            return val; 
         
-        var (r1,c1) = GetNumpadCoord(init);
-        var (r2,c2) = GetNumpadCoord(goal);
-        
-        string res = "";
-        res += c2 > c1 ? new string('>', c2 - c1) : "";
-        res += r1 > r2 ? new string('^', r1 - r2) : "";
-        res += c1 > c2 ? new string('<', c1 - c2) : "";
-        res += r2 > r1 ? new string('v', r2 - r1) : "";
-        res += "A";
-        return res;
+        char current = 'A';
+        long length = 0; 
+        for (int i = 0; i < code.Length; i++) 
+        {
+            List<string> moves = GetCommand(input, current, code[i]); 
+            if (robots == 0) 
+                length += moves[0].Length; 
+            else 
+                length += moves.Min(move => GetKeyPresses(move, robots - 1, keypad)); 
+            current = code[i];
+        } 
+        memo[(code,robots)] = length; 
+        return length; 
     }
 
-    private static char GetNumpadChar((int r,int c) coord) => coord switch
+    private static List<string> GetCommand(Dictionary<char, (int r, int c)> input, char start, char end)
     {
-        (0,0) => '7', 
-        (0,1) => '8',
-        (0,2) => '9',
-        (1,0) => '4',
-        (1,1) => '5',
-        (1,2) => '6',
-        (2,0) => '1',
-        (2,1) => '2',
-        (2,2) => '3',
-        (3,1) => '0',
-        (3,2) => 'A',
-        _ => throw new IndexOutOfRangeException()
+        Queue<(int r, int c, string path)> queue = [];
+
+        queue.Enqueue((input[start].r, input[start].c, string.Empty));
+        Dictionary<(int,int), long> distances = [];
+
+        if (start == end) return ["A"];
+
+        List<string> allPaths = [];
+        while (queue.Count != 0)
+        {
+            var cur = queue.Dequeue();
+            if ((cur.r, cur.c) == input[end]) 
+                allPaths.Add(cur.path + "A");
+
+            if (distances.TryGetValue((cur.r,cur.c), out var d) && d < cur.path.Length) 
+                continue;
+
+            foreach (var direction in dirs)
+            {
+                var (r, c) = (cur.r + direction.Value.r, cur.c + direction.Value.c);
+
+                if (!input.ContainsValue((r,c)) || (r,c) == input['#'])
+                    continue;
+                
+                var newPath = cur.path + direction.Key;
+                if (!distances.ContainsKey((r,c)) || distances[(r,c)] >= newPath.Length)
+                {
+                    queue.Enqueue((r, c, newPath));
+                    distances[(r,c)] = newPath.Length;
+                }
+            }
+        }
+
+        return allPaths.OrderBy(p => p.Length).ToList();
+    }
+
+    private static readonly Dictionary<(string code,long robots), long> memo = [];
+
+    private static readonly Dictionary<char, (int r,int c)> numpad = new()
+    {
+        {'7', (0,0)}, {'8', (0,1)}, {'9', (0,2)},
+        {'4', (1,0)}, {'5', (1,1)}, {'6', (1,2)},
+        {'1', (2,0)}, {'2', (2,1)}, {'3', (2,2)},
+        {'#', (3,0)}, {'0', (3,1)}, {'A', (3,2)}
     };
 
-    private static (int,int) GetNumpadCoord(char c)
+    private static readonly Dictionary<char, (int r,int c)> dirs = new()
     {
-        var row = c switch
-        {
-            '7' or '8' or '9' => 0,
-            '4' or '5' or '6' => 1,
-            '1' or '2' or '3' => 2,
-            _ => 3,
-        };
-        var col = c switch
-        {
-            '7' or '4' or '1' => 0,
-            '8' or '5' or '2' or '0' => 1,
-            _ => 2
-        };
-        return (row,col);
-    }
+        {'^', (-1,0)}, {'<', (0,-1)}, {'>', (0,1)}, {'v', (1,0)}
+    };
 
-    private static string TranslateKeypadCode(string code)
+    private static readonly Dictionary<char, (int r,int c)> keypad = new()
     {
-        string res = "";
-        code = "A" + code;
-        for(int i = 0; i < code.Length - 1; i++)
-            res += TranslateKeypad(code[i], code[i + 1]);
-        return res;
-    }
-
-    private static string TranslateKeypad(char init, char goal)
-    {
-        //     +---+---+
-        //     | ^ | A |
-        // +---+---+---+
-        // | < | v | > |
-        // +---+---+---+
-        
-        var (r1,c1) = GetKeypadChar(init);
-        var (r2,c2) = GetKeypadChar(goal);
-        // Right & down first
-        string res = "";
-        res += c2 > c1 ? new string('>', c2 - c1) : "";
-        res += r2 > r1 ? new string('v', r2 - r1) : "";
-        res += r1 > r2 ? new string('^', r1 - r2) : "";
-        res += c1 > c2 ? new string('<', c1 - c2) : "";
-        
-        res += "A";
-        return res;
-    }
-
-    private static (int,int) GetKeypadChar(char c)
-    {
-        var row = c switch
-        {
-            '^' or 'A' => 0,
-            _ => 1
-        };
-        var col = c switch
-        {
-            '<' => 0,
-            '^' or 'v' => 1,
-            _ => 2
-        };
-        return (row,col);
-    }
+        {'#', (0,0)}, {'^', (0,1)}, {'A', (0,2)},
+        {'<', (1,0)}, {'v', (1,1)}, {'>', (1,2)}
+    };
 }
