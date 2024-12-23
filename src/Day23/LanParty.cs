eac15663cb80c0df4feff5fbbdd88decd9d13e1e
Noud Savenije <noud.savenije@hotmail.com>
2024-12-23 07:47:49 +0100

    bronkerbosch 2

--- src/Day23/LanParty.cs
+++ src/Day23/LanParty.cs
@@ -70,6 +70,76 @@ static class Day23
 
     public static void Solve2()
     {
+        const string filePath = "src/Day23/23.in";
+        var connStrings = File.ReadAllLines(filePath);
+        Dictionary<string,HashSet<string>> connections = [];
+        foreach (string conn in connStrings)
+        {
+            var computers = conn.Split('-');
+
+            if (!connections.TryGetValue(computers[0], out _))
+            {
+                connections[computers[0]] = [];
+            }
+
+            connections[computers[0]].Add(computers[1]);
+
+            if (!connections.TryGetValue(computers[1], out _))
+            {
+                connections[computers[1]] = [];
+            }
+
+            connections[computers[1]].Add(computers[0]);
+        }
+
+        
+        List<string> maxClique = FindMaximumClique(connections).ToList();
+        maxClique.Sort();
+
+        Console.WriteLine(string.Join(',', maxClique));
     }
 
+    private static HashSet<string> FindMaximumClique(Dictionary<string,HashSet<string>> connections)
+    {
+        var r = new HashSet<string>();
+        var p = new HashSet<string>(connections.Keys);
+        var x = new HashSet<string>();
+
+        var maxClique = BronKerbosch(connections, r, p, x);
+
+        return maxClique;
+    }
+
+    //https://en.wikipedia.org/wiki/Bron%E2%80%93Kerbosch_algorithm#With_pivoting
+    private static HashSet<string> BronKerbosch(Dictionary<string,HashSet<string>> connections, HashSet<string> r, HashSet<string> p, HashSet<string> x)
+    {
+        HashSet<string> maxClique = [];
+        // if P and X are both empty then report R as a maximal clique
+        if (p.Count == 0 && x.Count == 0)
+        {
+            return r.Count > maxClique.Count ? [.. r] : maxClique;
+        }
+
+        // choose a pivot vertex u in P ⋃ X
+        var pivot = p.Concat(x).First();
+        // N(u)
+        var neighbors = connections.TryGetValue(pivot, out var conn) ? conn : [];
+        //for each vertex v in P \ N(u) do
+        foreach (var vertex in p.Except(neighbors).ToList())
+        {
+            // BronKerbosch2(R ⋃ {v}, P ⋂ N(v), X ⋂ N(v))
+            HashSet<string> newp = [.. p.Intersect(connections[vertex])];
+            HashSet<string> newx = [.. x.Intersect(connections[vertex])];
+
+            var clique = BronKerbosch(connections, r.Union([vertex]).ToHashSet(), newp, newx);
+            maxClique = clique.Count > maxClique.Count ? clique : maxClique;
+
+            // P := P \ {v}
+            p.Remove(vertex);
+            // X := X ⋃ {v}
+            x.Add(vertex);
+        }
+
+        return maxClique;
+    }
 }
